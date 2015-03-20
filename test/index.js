ea5b4e43b8510d7204aeaf71a5a36cc5d50b6ec7
stevenmiller888 <stevenmiller888@me.com>
2015-03-20 11:45:36 -0700

    added test/

---
+++ test/index.js
@@ -0,0 +1,21 @@
+
+/**
+ * Dependencies.
+ */
+
+var sherlockIntegrations = require('..');
+var assert = require('assert');
+
+/**
+ * Tests.
+ */
+
+describe('sherlock-integrations', function(){
+  beforeEach(function(){
+    
+  });
+
+  it('should ', function(done){
+    
+  });
+});
